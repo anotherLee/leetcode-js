982b0203c76c3b84da8c9877e8eb1814b326672e
limingru <18813070947@163.com>
2019-12-08 10:36:09 +0800

    使用正则 match 来添加方法

--- 557-reverse-words-in-a-string-iii/solution.js
+++ 557-reverse-words-in-a-string-iii/solution.js
@@ -1,10 +1,26 @@
 const input = "Let's take LeetCode contest"
 const output = "s'teL ekat edoCteeL tsetnoc"
 
-const solution = s => {
+const solution1 = s => {
   return s.split(' ').map(str => {
     return str.split('').reverse().join('')
   }).join(' ')
 }
 
-console.log(solution(input) === output)
+console.log('solution1: ', solution1(input) === output)
+
+const solution2 = s => {
+  return s.split(/\s/).map(str => {
+    return str.split('').reverse().join('')
+  }).join(' ')
+}
+
+console.log('solution2: ', solution2(input) === output)
+
+const solution3 = s => {
+  return s.match(/[\w']+/g).map(str => {
+    return str.split('').reverse().join('')
+  }).join(' ')
+}
+
+console.log('solution3: ', solution3(input) === output)
